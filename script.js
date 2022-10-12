31234dd9ae711e74fefc55a4090b858d35dc54cd
bonzo-coder <bonzo.code.maker@gmail.com>
2022-10-13 00:18:02 +0200

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let i=0;
 
-
 function drawPage () {
     let welcomeBox = document.createElement('h1');
     let messageBox = document.createElement('h2');
@@ -11,11 +10,17 @@ function drawPage () {
     let mainFlex = document.createElement('div');
     let mainWindow = document.createElement('div');
     mainFlex.id='mainFlex';
+    let button= document.createElement('button');
+    button.type='button';
+    button.id='buttonChoose';
+    button.value='Change grid size';
     mainWindow.id='mainWindow';
+    
     document.body.appendChild(welcomeBox);
     document.body.appendChild(messageBox);
     document.body.appendChild(mainFlex);
     mainFlex.appendChild(mainWindow);
+    mainFlex.appendChild(button);
     drawGrid();
 }
 
@@ -25,11 +30,28 @@ function drawGrid () {
          div.id=`box${i+1}`;
          div.className='boxes';
          div.innerHTML = '';
+         div.addEventListener('mouseover',(event) => 
+         {	
+             event.target.classList.add('colored');  
+         });
          mainWindow.appendChild(div);
          };
 }
 
+function chooseGrid () {
+
+}
+
+
+
+function buttonChoose () {
+    
+
+}
+
 drawPage();
+
+
 
 
 
