d25e45d719829af24d240f1ec8cd05ed849b3d38
bonzo-coder <bonzo.code.maker@gmail.com>
2022-10-18 02:35:44 +0200

    rainbow mode working. Code needs some cleanup. Add css for buttons. Check for bugs again

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-let s;
 let mainFlex;
 let mainWindow;
 let button= document.createElement('button');
+let colorInput=document.createElement('input');
+let btnColorMixer=document.createElement('button');
+let divUnderGrid = document.createElement('div');
+let buttonReset = document.createElement('button');
+let normalMode = true;
+let colorDraw;
+let colorArray;
 
-
-
-function drawPage (s) {
+function drawPage (s,i) {
     let pageBox = document.createElement('div');
     let welcomeBox = document.createElement('h1');
     let messageBox = document.createElement('h2');
-    let buttonReset = document.createElement('button');
-    let divUnderGrid = document.createElement('div');
-    welcomeBox.textContent='Etch a Sketch';
+    welcomeBox.textContent='NoteBook';
     messageBox.textContent='Feel free to draw in the grid below'
     pageBox.id='pageBox';
     welcomeBox.id='welcomeBox';
@@ -25,25 +27,53 @@ function drawPage (s) {
     buttonReset.type='button';
     buttonReset.id='buttonReset';
     buttonReset.textContent='Reset grid';
+    colorInput.type='color';
+    colorInput.id='colorInput';
+    btnColorMixer.type='button';
+    btnColorMixer.id='colorMixer';
+    btnColorMixer.textContent='Randomize'
     document.body.appendChild(pageBox);
     pageBox.appendChild(welcomeBox);
     pageBox.appendChild(messageBox);
     pageBox.appendChild(mainFlex);
-    mainFlex.appendChild(divUnderGrid);
-    divUnderGrid.appendChild(button);
-    divUnderGrid.appendChild(buttonReset);
     buttonReset.addEventListener('click', ()=> drawInitialGrid());
-
-    drawGrid(s);
-    chooseGrid();
+    drawGrid(s,i);
+    //chooseGrid();
+    btnColorMixer.addEventListener('click',()=>{
+        normalMode = !normalMode;
+    });
+    
+    colorDraw = 'black';
 }
 
-function drawGrid (s) {
+/*function colorMix(i) {
+    console.log(normalMode);
+    colorInput.addEventListener('change',()=>{
+    colorDraw = colorInput.value;
+    });
+    btnColorMixer.addEventListener('click', function() {
+        if(normalMode) {
+            colorDraw = colorInput.value;
+        }else if (!normalMode){
+            colorDraw =  colorArray[i-1];
+            console.log(colorArray[i]);
+        }
+        else {console.log(normalMode); }
+        });
+}*/
+
+function drawGrid (s,i) {
     console.log(s);
     mainWindow = document.createElement('div');
     mainWindow.id='mainWindow';
     mainFlex.appendChild(mainWindow);
+    mainFlex.appendChild(divUnderGrid);
+    divUnderGrid.appendChild(button);
+    divUnderGrid.appendChild(buttonReset);
+    divUnderGrid.appendChild(btnColorMixer);
+    divUnderGrid.appendChild(colorInput);
     let div = document.createElement('div');
+    colorArray = new Array(s*s);
     for (let i=0; i<s*s; i++) {
          div = document.createElement('div');
          div.id=`box${i+1}`;
@@ -51,9 +81,27 @@ function drawGrid (s) {
          div.innerHTML = '';
          div.style.width=`${(400-2*s)/s}px`;
          div.style.height=`${(400-2*s)/s}px`;
-         div.addEventListener('mouseover',(event) => 
+         colorArray[i]='#'+Math.floor(Math.random()*16777215).toString(16);
+         div.addEventListener('mouseover',() => 
          {	
-             event.target.classList.add('colored');  
+            //function colorMix(i) {
+                console.log(normalMode);
+                colorInput.addEventListener('change',()=>{
+                colorDraw = colorInput.value;
+                });
+                //btnColorMixer.addEventListener('click', function() {
+                    if(normalMode) {
+                        colorDraw = colorInput.value;
+                        document.getElementById(`box${i+1}`).style.backgroundColor = colorDraw;
+                    }else if (!normalMode){
+                        colorDraw =  colorArray[i-1];
+                        console.log(colorArray[i]);
+                        document.getElementById(`box${i+1}`).style.backgroundColor = colorDraw;
+                    }
+                    else {console.log(normalMode); }
+                  //  });
+           // }
+           // document.getElementById(`box${i+1}`).style.backgroundColor = colorDraw;
          });
          mainWindow.appendChild(div);
          };
@@ -101,7 +149,8 @@ function drawGrid (s) {
         else {
            
         }
-         
+         console.log(colorArray);
+        
    // chooseGrid();
 }
 
@@ -115,20 +164,20 @@ function cleanGrid () {
     main.remove('pageBox');
 }
 
-function chooseGrid () {
+//function chooseGrid () {
     button.addEventListener('click', function() {
         s = parseInt(prompt("Grid size: Type a number from 1 to 100"));
         if (s>0 && s<101) {
             i=0;
             cleanGrid();
-            drawPage(s);
+            drawPage(s,i);
         }
         else { 
             alert('Try again. Type an integer from 1 to 100');
             drawInitialGrid();
         }
     });
-}
+//}
 
 //function buttonChoose () {
     
